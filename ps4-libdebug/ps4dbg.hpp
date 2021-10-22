d48872b23561f7eef6dc38c630d6a99dcf68de84
ac2pic <ac2pic@gmail.com>
2021-10-22 12:25:39 -0500

    Created basic PS4DBG header file - Defines subset of all api calls

---
+++ ps4-libdebug/ps4dbg.hpp
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <vector>
+#include <string>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "ps4process.hpp"
+
+#pragma once
+
+class PS4DBG {
+    private:
+        struct sockaddr_in m_Server;
+    public:
+        PS4DBG(const std::string &ip);
+		~PS4DBG();
+		void Connect();
+		void Disconnect();
+        
+		std::vector<PS4Process> GetProcessList();
+};
